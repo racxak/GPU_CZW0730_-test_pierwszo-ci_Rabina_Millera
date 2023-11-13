1d0d1fc5ca3c1e01094ec2d990ab2ccbd5c7f8f8
racxak <92050973+racxak@users.noreply.github.com>
2023-11-13 21:42:26 +0100

    primality test sequentially

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import numpy as np
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Function to check if n is prime, used for the Rabin Miller test
+def is_prime(n, k=7):  # number of tests = k
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0 or n < 2:
+        return False
 
+    s, d = 0, n - 1
+    while d % 2 == 0:
+        s += 1
+        d //= 2
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    for _ in range(k):
+        a = np.random.randint(2, n-1, dtype=np.int64).item()
+        x = pow(a, d, n)
+        if x == 1 or x == n - 1:
+            continue
+        for r in range(s):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
 
+# Function to test primes sequentially
+def sequential_prime_test(numbers):
+    results = []
+    for number in numbers:
+        result = is_prime(number)
+        results.append(result)
+        print(f"{number} is {'prime' if result else 'not prime'}")
+    return results
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# List of numbers to test for primality
+numbers = [999331, 1111111111111111111, 982451653]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Start the timer
+start_time = time.time()
+
+# Test the numbers for primality sequentially
+results = sequential_prime_test(numbers)
+
+# End the timer
+end_time = time.time()
+print(f"Time taken: {end_time - start_time} seconds")
